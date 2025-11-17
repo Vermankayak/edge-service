f94dd5e762b27e8fafa041afb3a4a8ea7debdbfa
Nihar Kuchroo <niharku@gmail.com>
2025-11-17 22:17:42 +0530

    Added files to edge-service

---
+++ src/main/java/com/polarbookshop/edgeservice/user/UserController.java
@@ -0,0 +1,30 @@
+package com.polarbookshop.edgeservice.user;
+
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @GetMapping("user")
+    public Mono<User> getUser(){
+        return ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication)
+                .map(authentication -> {
+                    return (OidcUser) authentication.getPrincipal();
+                }).map(oidcUser -> {
+                    return new User(oidcUser.getPreferredUsername(),
+                            oidcUser.getGivenName(),
+                            oidcUser.getFamilyName(),
+                           // List.of("employee", "customer"),
+                            oidcUser.getClaimAsStringList("roles")
+                    );
+                });
+    }
+}
